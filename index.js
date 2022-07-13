6f5f2cfbba314eb4d3db0c68c18790c364d5acf9
Pushparaj <ppushparaj1998@gmail.com>
2022-07-13 11:49:53 +0530

    ip added

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser(process.env.COOKIESECRET))
 
+app.use(function (req, res, next) {
+    const allowedOrigins = ["http://localhost:4200", "http://localhost:4300"];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next();
+})
+
 const bachelorRouter = require('./routers/bachelors');
 const budgetRouter = require('./routers/budget');
 
@@ -15,7 +27,7 @@ app.use(bachelorRouter);
 app.use(budgetRouter);
 
 const port = 4002 || process.env.PORT;
-console.log("process.env.PORT", process.env.PORT)
-app.listen(port, ()=> {
+
+app.listen(port, "10.1.41.241", ()=> {
     console.log("Kutty Budget running on the port : ", port)
 })
